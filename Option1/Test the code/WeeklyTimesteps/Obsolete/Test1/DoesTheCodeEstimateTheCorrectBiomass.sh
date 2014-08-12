4d614db4c559191047b9b0a696dd7bf6a2bbeb2b
Marco Kienzle <Marco.Kienzle@gmail.com>
2014-08-13 06:52:47 +1000

    few changes

--- Option1/Test the code/WeeklyTimesteps/Obsolete/Test1/DoesTheCodeEstimateTheCorrectBiomass.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-R --vanilla < SimulatePopDynamic.R
-
-# to square root of catch 
-matlab -nodesktop -nosplash -r "SimulatedDatasets; MbTigerPrawnParameters; par = ones(1,2); DelayDifference; csvwrite('Results/EstimatedBiomassByFittingSqrtOfCatch.csv', Biomass); exit"
-
-R --vanilla < CompareEstimate2Simulated.R
